1a30a839d6cc038d3d2f8c501c4c84f32cf476d6
Evan You <yyx990803@gmail.com>
2016-12-13 18:10:54 -0500

    better build script for vue

--- src/vue/ssr.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-const app = require('./app')
-const template = fs.readFileSync('./ssr-template.html', 'utf-8')
-const renderer = require('vue-server-renderer').createRenderer()
-
-renderer.renderToString(app, (err, res) => {
-  const output = template.replace('<!-- OUTPUT -->', res)
-  fs.writeFileSync('./ssr.html', output)
-})
