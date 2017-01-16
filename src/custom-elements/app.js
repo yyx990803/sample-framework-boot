1375828f911488166b12d0c50d6890c3c37c8444
Evan You <yyx990803@gmail.com>
2017-01-16 10:01:29 -0500

    actual use CSR for custom elements

--- src/custom-elements/app.js
+++ src/custom-elements/app.js
@@ -19,6 +19,33 @@
 
 'use strict';
 
+customElements.define('comments-app', class extends HTMLElement {
+  constructor () {
+    super();
+  }
+
+  connectedCallback () {
+    if (typeof window.__comments === 'undefined') {
+      console.warn('Unable to find comment data. Bailing.');
+      return;
+    }
+
+    const comments = document.createElement('post-comments');
+    comments._data = window.__comments;
+
+    this.innerHTML = `<section class="post">
+      <h1 class="post__title">A post about priorities and scheduling</h1>
+      <a href="#" class="post__link">http://example.com/post</a>
+      <div class="post__description">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed orci mauris, euismod quis imperdiet eu, lacinia in leo. Mauris in orci sit amet eros finibus molestie. Vivamus laoreet, nibh eget ultrices consequat, leo mauris suscipit libero, eget feugiat enim turpis ac neque. Nam venenatis dolor ac posuere rhoncus.</p>
+        <p>In eget finibus sapien, sit amet tempus augue. Pellentesque eu interdum nulla, sit amet dictum turpis. Sed ut lorem quis purus aliquet egestas. Nulla id risus ex. Morbi et lacinia risus. Integer eget ornare arcu. Donec purus est, dignissim a ante a, lacinia placerat massa. Donec quis lacinia lorem.</p>
+      </div>
+    </section>`;
+
+    this.querySelector('.post').appendChild(comments);
+  }
+})
+
 customElements.define('post-comments', class extends HTMLElement {
   constructor () {
     super();
